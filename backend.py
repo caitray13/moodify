f8c0a3527426b005026af46bbdfe297fa3ef195a
Caitlin <cai.ray13@gmail.com>
2021-04-16 13:10:04 +0100

    sticking in a class

--- backend.py
+++ backend.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
 import json
 import os
-import spotify
+from spotify import SpotifyService
 
 app = Flask(__name__)
 
@@ -10,9 +10,10 @@ HOST = os.getenv('HOST', 'localhost')
 
 @app.route("/")
 def home():
-    request_data = {'results': spotify.recently_played}
+    request_data = {'results': spotify.get_recently_played()}
     return request_data
 
 
 if __name__ == '__main__':
+    spotify = SpotifyService(scope='user-read-recently-played')
     app.run(host=HOST, port=PORT)
--- spotify.py
+++ spotify.py
@@ -1,16 +1,23 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-scope = "user-read-recently-played"
+from typing import Dict, List
 
-spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
-results = spotify.current_user_recently_played()
-recently_played = []
-for idx, item in enumerate(results['items']):
-    track = item['track']
-    recently_played_song = {}
-    recently_played_song['artists'] = track['artists'][0]['name']
-    recently_played_song['name'] = track['name']
-    recently_played.append(recently_played_song)
-    print(idx, recently_played_song['artists'], " – ", recently_played_song['name'])
+class SpotifyService():
+    def __init__(self, scope) -> None:
+        self.scope = scope
+        self.spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+
+    def get_recently_played(self) -> List[Dict]:
+        r = self.spotify.current_user_recently_played()
+        recently_played = []
+        for idx, item in enumerate(r['items']):
+            track = item['track']
+            recently_played_song = {}
+            recently_played_song['artists'] = track['artists'][0]['name']
+            recently_played_song['name'] = track['name']
+            recently_played.append(recently_played_song)
+            print(idx, recently_played_song['artists'],
+                  " – ", recently_played_song['name'])
+        return recently_played
