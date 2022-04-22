c71d5eca77b37abae2395d696f25df8c5a0d0384
Caitlin <cai.ray13@gmail.com>
2022-04-22 12:14:22 +0100

    making some track attributes optional

--- track.py
+++ track.py
@@ -1,16 +1,17 @@
 from dataclasses import dataclass, field
-from typing import Any, Optional
+from typing import Any, Optional, List
 
 
 @dataclass
 class Track:
     """ Object class for represeting a track on Spotify"""
-    track_id: str
     track_uri: str
     track_name: str
     artist_name: str
-    artist_id: str
-    artist_uri: str
+    lyrics: Optional[List[str]] = field(default_factory=list)
+    track_id: Optional[str] = None
+    artist_id: Optional[str] = None
+    artist_uri: Optional[str] = None
     popularity: str = field(default=None)
     duration_ms: float = field(default=None)
 
